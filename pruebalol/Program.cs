0d1235b424234627945ecf94f4a21e7be4a6d7e2
bo0p_V1 <thiago11123xd@gmail.com>
2025-05-07 19:35:22 -0300

    por si las dudas

--- pruebalol/Program.cs
+++ pruebalol/Program.cs
@@ -1,1 +1,78 @@
-﻿
+﻿//hola puedo caer asi a la prueba?
+
+using pruebalol;
+using System.Runtime.CompilerServices;
+
+provincia prov = new provincia();
+ciudad ciudadlol = new ciudad();
+List<provincia> provincias = new();
+List<ciudad> ciudades = new();
+
+do
+{
+    Console.WriteLine("ingrese el nombre de la provincia!");
+    prov.nombre = Console.ReadLine();
+    if (prov.nombre != null)
+    {
+        carga(prov);
+    }
+    else
+    {
+        Console.WriteLine("terminando la carga de provincias........");
+        break;
+    }
+
+}
+while (prov.nombre != null);
+
+Console.WriteLine("aprete para continuar");
+Console.ReadKey();
+Console.Clear();
+
+
+void carga(provincia provinciad)
+{
+    provinciad.nombre = prov.nombre;
+    Console.WriteLine("ingrese el nombre del gobernador!");
+    provinciad.gobernador = Console.ReadLine();
+    Console.WriteLine("ingrese la region!");
+    provinciad.region = Console.ReadLine();
+    provincias.Add(provinciad);
+    do
+    {
+        Console.WriteLine("ingrese el nombre de la ciudad");
+        ciudadlol.nombre = Console.ReadLine();
+        if (ciudadlol.nombre != null)
+        {
+            cargaciudad(ciudadlol);
+        }
+        else
+        {
+            Console.WriteLine("terminando la carga de ciudades........");
+            break;
+        }
+
+    }
+    while (ciudadlol.nombre != null);
+
+}
+
+
+
+void cargaciudad(ciudad ciud)
+{
+    ciud.nombre = ciudadlol.nombre;
+    Console.WriteLine("ingrese la cantidad de habitantes de la ciudad !");
+    ciud.CantHabitantes=int.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese la distancia en km de la ciudad!");
+    ciud.SuperficieKM =int.Parse(Console.ReadLine());
+    ciudades.Add(ciud);
+
+}
+
+
+
+
+
+
+//hola, no, desaprobado.
---
+++ pruebalol/ciudad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebalol
+{
+    public class ciudad
+    {
+
+        public string nombre {get;set;}
+        public int CantHabitantes { get; set; }
+        public double SuperficieKM { get; set; }
+    }
+}
